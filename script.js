125c780a6f16debc40419c0e73e5c679d477fef1
lucasrodriges <lucasrodrigues7691@gmail.com>
2022-09-30 21:39:35 -0300

    modified-home

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
 /*  Abre e fecha o menu quando clicar no icone HAMBURGER*/
 
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 
 for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-  })
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
 }
 /*quando clicar em um item do menu , esconder o menu */
 
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll("nav ul li a");
 
 for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
 }
 
 /* mudar o header da página quando der scroll */
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
-    header.classList.add('scroll')
+    header.classList.add("scroll");
   } else {
     // menor que a altura do header
-    header.classList.remove('scroll')
+    header.classList.remove("scroll");
   }
 }
 
 /* Testimonials carousel slider swiper */
 
-const swiper = new Swiper('.testimonials', {
+const swiper = new Swiper(".testimonials", {
   keyboard: true,
   mousewheel: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   pagination: {
-    el: '.swiper-pagination'
+    el: ".swiper-pagination",
   },
   breakpoints: {
     767: {
       slidesPerView: 2,
-      setWrapperSize: true
-    }
-  }
-})
+      setWrapperSize: true,
+    },
+  },
+});
 
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
-  origin: 'top',
-  distance: '30px',
+  origin: "top",
+  distance: "30px",
   duration: 700,
-  reset: true
-})
+  reset: true,
+});
 
 scrollReveal.reveal(
   `#home .image, #home .text,
@@ -69,49 +69,136 @@ scrollReveal.reveal(
   footer .brand, footer .social
   `,
   { interval: 100 }
-)
+);
 
 /* Botão voltar para o topo */
-const backToTopButton = document.querySelector('.back-to-top')
+const backToTopButton = document.querySelector(".back-to-top");
 
 function backToTop() {
   if (window.scrollY >= 700) {
-    backToTopButton.classList.add('show')
+    backToTopButton.classList.add("show");
   } else {
-    backToTopButton.classList.remove('show')
+    backToTopButton.classList.remove("show");
   }
 }
 
 /* Menu ativo conforme a seção visível na página */
-const sections = document.querySelectorAll('main section[id]')
+const sections = document.querySelectorAll("main section[id]");
 function activateMenuAtCurrentSection() {
-  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
 
   for (const section of sections) {
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionId = section.getAttribute('id')
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
 
-    const checkpointStart = checkpoint >= sectionTop
-    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
 
     if (checkpointStart && checkpointEnd) {
       document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.add('active')
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
     } else {
       document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.remove('active')
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
     }
   }
 }
 
 /* When Scroll */
-window.addEventListener('scroll', function () {
-  changeHeaderWhenScroll()
-  backToTop()
-  activateMenuAtCurrentSection()
-})
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop();
+  activateMenuAtCurrentSection();
+});
 
+/* Animation*/
+const elts = {
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2"),
+};
 
+const texts = [
+  "HTML",
+  "CSS",
+  "React.js",
+  "TypeScript",
+  "JavaScript",
+  "Context API",
+  "Next.js",
+  "Node.js",
+  "Styled-components",
+  "Front-end",
+];
+
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+    cooldown = cooldownTime;
+    fraction = 1;
+  }
+
+  setMorph(fraction);
+}
+
+function setMorph(fraction) {
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+  morph = 0;
+
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
+
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 1000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+    if (shouldIncrementIndex) {
+      textIndex++;
+    }
+
+    doMorph();
+  } else {
+    doCooldown();
+  }
+}
+
+animate();
